ed937e3214fa4341e1896d654a738b184b0b6561
Kamaljot Singh <48052931+Deathstrock404@users.noreply.github.com>
2020-04-07 03:23:03 +0530

    practice python code

---
+++ math.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+#math
+
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 - 50 * 50 /50) #BODMAS
+print(50 ** 2) #exponents
+print(50 % 6) #modules to find the remainder of two nos
+print(50 / 6) #we are going to get a remainder and the result is going to be in double
+print(50 // 6) #this means that we want no remainders 
